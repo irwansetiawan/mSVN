47986de5b58f8698639e6b3cef7135b5306932d9
Irwan Setiawan <irwanscreative@gmail.com>
2013-11-26 15:02:18 +0800

    Check for SVN url to get project and branch name

--- checkout.php
+++ checkout.php
@@ -8,19 +8,34 @@ require_once dirname(__FILE__).'/config.php';
 $svnUrl = filter_input(INPUT_POST, 'svnurl');
 $res['svnurl'] = $svnUrl;
 
-$dirname = strtolower(basename($svnUrl));
-$res['dirname'] = $dirname;
+$projectName = $branchName = NULL;
 
-$outputLines = shell_exec('cd '.$baseDir.'; '.
-                          'svn checkout --username '.$svnUsername.' --password '.$svnPassword.' --non-interactive '.
-                                        $svnUrl.' '.$dirname.' 2>&1;');
+// check if svn url format is correct
+if (preg_match('/\/([^\/]*)\/trunk$/', $svnUrl, $matches)) { // is trunk copy
+	$projectName = $matches[1];
+	$branchName = 'trunk';
+} else if (preg_match('/\/([^\/]*)\/branches\/([^\/]*)\/?$/', $svnUrl, $matches)) { // is branch copy
+	$projectName = $matches[1];
+	$branchName = $matches[2];
+} else {
+	$res['status'] = 'error';
+	$res['error'] = 'Invalid SVN URL';
+}
 
-shell_exec('cd '.$baseDir.'; '.
-           'touch '.$dirname.'/'.$projectInitFile);
+if (!empty($projectName) && !empty($branchName)) {
+	$dirname = strtolower($projectName.'-'.$branchName);
+	$res['dirname'] = $dirname;
 
-$res['status'] = 'ok';
-$res['output'] = $outputLines;
+	$outputLines = shell_exec('cd '.$baseDir.'; '.
+	                          'svn checkout --username '.$svnUsername.' --password '.$svnPassword.' --non-interactive '.
+	                                        $svnUrl.' '.$dirname.' 2>&1;');
 
+	shell_exec('cd '.$baseDir.'; '.
+	           'touch '.$dirname.'/'.$projectInitFile);
+
+	$res['status'] = 'ok';
+	$res['output'] = $outputLines;
+}
 
 echo json_encode($res);
 
